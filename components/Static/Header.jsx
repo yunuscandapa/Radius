666b01c116d85370e985a94f4d766aaa35fbf087
yunuscandapa <125770844+yunuscandapa@users.noreply.github.com>
2023-06-30 20:29:11 +0300

    Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -14,7 +14,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
                         <img width="32" className="rounded-full icon-glow" src="/img/logo.jpg" />
-                        <p className=" font-extrabold ml-1 text-2xl">Astra Bot</p>
+                        <p className=" font-extrabold ml-1 text-2xl">Radius Bot</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
                 </div>
@@ -83,7 +83,7 @@ const Header = ({ $, NavItems }) => {
                                 width="48" height="48"
                             />
                             <p className="invisible md:visible text-xl text-white font-semibold">
-                              <a href="/"><span className="text-amber-400">Astra</span>Bot</a>
+                              <a href="/"><span className="text-amber-400">Radius</span>Bot</a>
                             </p>
                         </div>
                         <ul className="hidden lg:flex items-center space-x-4">
