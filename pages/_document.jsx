69c7c54a1d245fe09cd2c86fb674f43b04c4e828
yunuscandapa <125770844+yunuscandapa@users.noreply.github.com>
2023-06-30 21:37:50 +0300

    _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Hi Do you need a moderation bot? Then you are in the right place. You can call the bot by pressing the Invite button."
           />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:card" content="Radius" />
+          <meta name="twitter:site" content="@RadiusBot" />
+          <meta name="twitter:creator" content="@RadiusBot" />
+          <meta property="og:url" content="https://RadiusBot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="RadiusBot" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -29,11 +29,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="Radius bot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Radius"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
